3b574e1e00e6e63fdbcc385d8e56e04b70910789
Samantha Li <sl5406021@gmail.com>
2024-07-30 10:48:52 -0400

    formatting

---
+++ 2QuestionApp/exp2.swift
@@ -0,0 +1,18 @@
+//
+//  exp2.swift
+//  2QuestionApp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct exp2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    exp2()
+}
---
+++ 2QuestionApp/exp3.swift
@@ -0,0 +1,18 @@
+//
+//  exp3.swift
+//  2QuestionApp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct exp3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    exp3()
+}
